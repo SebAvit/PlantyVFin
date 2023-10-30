43b675865f6dbd1ad8984bf0ec671641717fdf64
Sébastien Avit <sebastien.avit98gmail.com>
2023-10-30 12:46:49 +0100

    Update functions.php

--- wordpress/wp-content/themes/planty-child/functions.php
+++ wordpress/wp-content/themes/planty-child/functions.php
@@ -11,10 +11,9 @@ function theme_enqueue_styles()
 add_filter( 'wp_nav_menu_items', 'add_extra_item_to_nav_menu', 10, 2 );
 function add_extra_item_to_nav_menu( $items, $args ) {
     if (is_user_logged_in() && 'primary-menu') {
-        $items .= ' <li id="menu-item-62" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-62><a href="'. admin_url() .'">Admin</a>
-                    </li>';
+        $items .= '<li id="menu-item-62" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-62"><a href="' . admin_url() . '">ALLO</a></li>';
     }
     return $items;
 }
-apply_filters( 'wp_nav_menu_items', 'wp_nav_menu', $args->menu == 2);
+apply_filters( 'wp_nav_menu_items', 'wp_nav_menu', 'primary-menu');
 ?>
